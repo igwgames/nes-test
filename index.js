6a65064c1b6e3857411e7c4dc3618b6fd3819d46
cppchriscpp <admin@cpprograms.net>
2022-05-22 23:21:19 -0400

    Dumb dumb dumb

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
     NesRomFile: require('./js-wrapper/nes-rom-file'),
     NesEmulator: require('./js-wrapper/nes-emulator'),
-    JasmineMatchers: require('./util/jasmine-matchers').JasmineMatchers
+    JasmineMatchers: require('./util/jasmine-matchers')
 };
